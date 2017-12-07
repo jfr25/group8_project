15d5c8c09dbee1ae391a6ab0de4d724c2c4b00b9
Ryan Faris <rzf5143@psu.edu>
2017-12-07 10:51:12 -0500

    Small Changes to include more in popups

--- ShinyApp.R
+++ ShinyApp.R
@@ -36,13 +36,22 @@ search_events <- function(location,date,category,key){
   event_data <- xmlParse(url)
   event_data <- xmlToList(event_data)$event
   event_data <- as.data.frame(do.call('rbind',event_data),nrow=length(event_data),ncol=length(event_data[[1]]),stringsAsFactors = FALSE)
-  event_data <- data.frame(title = as.character(unlist(event_data$title)), lon = as.numeric(unlist(event_data$longitude)), 
-                           lat = as.numeric(unlist(event_data$latitude)),url = as.character(unlist(event_data$url)),
-                           venue = as.character(unlist(event_data$venue_name)), venue_url = as.character(unlist(event_data$venue_url)))
+  event_data <- data.frame(title = as.character(event_data$title), lon = as.numeric(event_data$longitude), 
+                           lat = as.numeric(event_data$latitude),url = as.character(event_data$url),
+                           description = as.character(event_data$description),venue = as.character(event_data$venue_name), 
+                           venue_url = as.character(event_data$venue_url),start_time = as.character(event_data$start_time), 
+                           stop_time = as.character(event_data$stop_time), venue_address = as.character(event_data$venue_address),
+                           city = as.character(event_data$city_name),zip = as.character(event_data$postal_code))
+  
   popups <- paste(sep = "<br/>",
                   paste(sep = '', '<b><a href=', event_data$url, '>',  event_data$title, ' </a></b>'),
-                  paste(sep = '', '<b><a href=', event_data$venue_url, '>',  event_data$venue, ' </a></b>')
-  )
+                  event_data$start_time,
+                  event_data$description,
+                  paste(sep = '', '<b><a href=', event_data$venue_url, '>',  event_data$venue, ' </a></b>'),
+                  event_data$venue_address,
+                  event_data$city,
+                  event_data$zip)
+  
   event_data <- cbind(event_data,popups)
   class(event_data) <- "events"
   return(event_data)
@@ -58,8 +67,8 @@ header <- dashboardHeader(
 body <- dashboardBody(
   fluidRow(
     column(width = 9,
-           box(width = '100%', height = '100%', solidHeader = TRUE,
-               leafletOutput("Map", height = 500)
+           box(width = '700%', height = '100%', solidHeader = TRUE,
+               leafletOutput("Map", width = '100%', height = 600)
            )
     ),
     column(width = 3,
@@ -76,7 +85,7 @@ body <- dashboardBody(
 )
 
 ui <- dashboardPage(
-  skin=c('black'),
+  skin="purple",
   header,
   dashboardSidebar(disable = TRUE),
   body
